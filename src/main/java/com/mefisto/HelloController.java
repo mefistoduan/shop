2aa86fdd2f40ddd3f08aed3924d2686cb1716b72
Changpeng Duan <mefistoduan@users.noreply.github.com>
2018-08-28 22:50:45 +0800

    0.0.1
    接收数据

--- src/main/java/com/mefisto/HelloController.java
+++ src/main/java/com/mefisto/HelloController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import com.mefisto.Entity;
+
 import javax.validation.Valid;
 
 @RestController
@@ -21,4 +26,28 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @RequestMapping(value = "/data")
+//    public List<Entity> data() {
+//        List<Entity> list = new ArrayList<Entity>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            Entity Entity = new Entity();
+//            Entity.setId(i + 1);
+//            Entity.setName("springboot" + i);
+//            Entity.setSex("male");
+//            Entity.setAge(i + 1);
+//            Entity.setRole("developer");
+//
+//            list.add(Entity);
+//        }
+//
+//        return list;
+//    }
+    public String rtnCode() {
+        return "{rtnCode:0,rs:[{springboot:444}],rtnMemo:123}";
+    }
+
 }
+
+//{"rtnCode":0,"rs":[],"rtnMemo":"\u6b63\u5e38"}
